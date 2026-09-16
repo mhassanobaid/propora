e21ca8fdb4552d8210dd0d420c943b5641b562b9
mhassanobaid <hassanobaidkhan999160@gmail.com>
2026-09-16 20:44:20 +0500

    bug: resolve route issue of search listing

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -50,7 +50,7 @@ export default function Search() {
     const fetchListings = async () => {
       setLoading(true);
       const searchQuery = urlParams.toString();
-      const res = await fetch(`/api/listing/get?${searchQuery}`);
+      const res = await fetch(`/api/v1/listings/index?${searchQuery}`);
       const data = await res.json();
       setListings(data);
       setLoading(false);
